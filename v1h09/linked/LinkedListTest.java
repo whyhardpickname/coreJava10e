91e7a8f9633668b368d78a732ea371e33e879117
whyhardpickname <13zxchen3@gmail.com>
2020-08-17 07:32:55 +0800

    linkedList

---
+++ v1h09/linked/LinkedListTest.java
@@ -0,0 +1,27 @@
+package linked;
+
+import java.util.*;
+
+public class LinkedListTest
+{
+	public static void main(Sting[] args)
+	{
+		List<String> linked1 = new LinkedList<String>();
+		linked1.add("A");
+		linked1.add("C");
+		linked1.add("E");
+		
+		List<String> linked2 = new LinkedList<String>();
+		linked2.add("B");
+		linked2.add("D");
+		linked2.add("F");	
+		linked2.add("G");
+		
+			
+		ListIterator<String> iter1 = linked1.listIterator();
+		ListIterator<String> iter2 = linked1.listIterator();
+		
+		//to do
+		
+	}
+}
