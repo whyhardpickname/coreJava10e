006296e243dc456c09f8586f165ff1c044425138
whyhardpickname <13zxchen3@gmail.com>
2020-08-24 07:13:21 +0800

    Sieve.java

--- v1ch09/sieve/Sieve.java
+++ v1ch09/sieve/Sieve.java
@@ -6,6 +6,36 @@ public class Sieve
 {
 	public static void main(String[] args)
 	{
-		//toDo
+		int n = 2000000;
+		BitSet primes = new BitSet(n + 1);
+		for (int i = 2; i <= n; i++)
+		{
+			primes.set(i);
+		}
+		
+		int i = 2;
+		int counter = 0;
+		int k = 2;
+		while (i * i <= n)
+		{
+			if (primes.get(i))
+			{
+				k = 2 * i;
+				while (k <= n)
+				{
+					primes.clear(k);
+					k += i;
+				}
+			}
+			i++;
+		}
+		for (int j = 0; j <= n; j++)
+		{
+			if (primes.get(j))
+			{
+				counter++;
+			}
+		}
+		System.out.println(counter);
 	}
 }
