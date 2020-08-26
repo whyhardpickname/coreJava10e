b8da4bcf3dfe256fa6ba9ced76d2d875480cf37b
whyhardpickname <13zxchen3@gmail.com>
2020-08-27 07:36:51 +0800

    multi thread

---
+++ v1ch14/unsynch/UnsynchBankTest.java
@@ -0,0 +1,79 @@
+package unsynch;
+
+import java.util.*;
+
+public class UnsynchBankTest
+{
+	public static final int NACCOUNTS = 100;
+	public static final double INITIAL_BALANCE = 1000;
+	public static final double MAX_AMOUNT = 1000;
+	public static final int DELAY = 10;
+	
+	public static void main(String[] args)
+	{
+		Bank bank = new Bank(NACCOUNTS, INITIAL_BALANCE);
+		for (int i = 0; i < NACCOUNTS; i++)
+		{
+			int fromAccount = i;
+			Runnable r = () -> 
+			{
+				try
+				{
+					while(true)
+					{
+						int toAccount = (int) (bank.size() * Math.random());
+						double amount = MAX_AMOUNT * Math.random();
+						bank.transfer(fromAccount, toAccount, amount);
+						Thread.sleep((int) (DELAY * Math.random()));
+					}
+				}
+				catch (InterruptedException e)
+				{
+				}
+			};
+			Thread t = new Thread(r);
+			t.start();
+		}
+	}
+	
+}
+
+class Bank
+{
+	
+	private final double[] accounts;
+	
+	Bank(int n, double initialBalance)
+	{
+		accounts = new double[n];
+		Arrays.fill(accounts, initialBalance);
+	}
+	
+	public void transfer(int from, int to, double amount)
+	{
+		if (accounts[from] < amount)
+		{
+			return;
+		}
+		System.out.print(Thread.currentThread());
+		accounts[from] -= amount;
+		System.out.printf("%10.2f from %d to %d", amount, from, to);
+		accounts[to] += amount;
+		System.out.printf("Total balance: %10.2f%n", getTotalBalance());
+	}
+	
+	public double getTotalBalance()
+	{
+		double total = 0;
+		for (int i = 0; i < accounts.length; i++)
+		{
+			total += accounts[i];
+		}
+		return total;
+	}
+	
+	public int size()
+	{
+		return accounts.length;
+	}
+}
